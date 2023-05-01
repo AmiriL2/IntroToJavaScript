36a351ea942b8efc551c9f5de94276155d0ef9b6
AmiriL2 <amirilayne@gmail.com>
2023-05-01 14:53:01 -0400

    stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,12 +1,39 @@
-function start () {
-    alert("You clicked the start button")
+let seconds = 0
+let milliseconds = 0
+let minutes = 0
 
+let digits = document.getElementById("digits")
+
+let interval;
+
+function start () {
+    reset()
+
+    interval = setInterval(function () {
+        milliseconds = milliseconds + 10
+        
+        if(milliseconds >= 1000){
+            seconds = seconds + 1
+            milliseconds = 0
+        }
+         if (seconds >= 60){
+            minutes = minutes + 1
+            seconds = 0
+         }
+ 
+         digits.innerHTML = String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0") + ":" + milliseconds/10
+
+    }, 10)
 }
 
 function stop () {
-    alert("You clicked the stop button ")
+    clearInterval(interval)
 }
 
 function reset () {
-    alert("You clicked the reset button")
-}
+    clearInterval(interval)
+    milliseconds = 0
+    seconds = 0
+    minutes = 0
+      digits.innerHTML = String(minutes).padStart(2,"0") + ":" + String(seconds).padStart(2, "0") + ":" + String(milliseconds).padStart(2, "0")
+    }
