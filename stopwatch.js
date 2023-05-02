77d2cbaf63d992338d97be8227eda417ac150e3a
AmiriL2 <amirilayne@gmail.com>
2023-05-02 14:53:20 -0400

    lap timer

--- stopwatch.js
+++ stopwatch.js
@@ -2,12 +2,15 @@ let seconds = 0
 let milliseconds = 0
 let minutes = 0
 
+let startButton = document.getElementById("start-btn")
+
 let digits = document.getElementById("digits")
 
 let interval;
 
 function start () {
-    reset()
+
+    startButton.disabled = true
 
     interval = setInterval(function () {
         milliseconds = milliseconds + 10
@@ -27,6 +30,10 @@ function start () {
 }
 
 function stop () {
+
+    startButton.disabled = false
+    digits.innerHTML = String(minutes).padStart(2,"0") + ":" + String(seconds).padStart(2, "0") + ":" + milliseconds/10
+
     clearInterval(interval)
 }
 
@@ -36,4 +43,16 @@ function reset () {
     seconds = 0
     minutes = 0
       digits.innerHTML = String(minutes).padStart(2,"0") + ":" + String(seconds).padStart(2, "0") + ":" + String(milliseconds).padStart(2, "0")
+   
+
+    const myList = document.getElementById("myList")
+    myList.innerHTML = ""
     }
+
+function lapse () {
+    let MyList = document.getElementById("myList")
+
+    let NewListItem = document.createElement('li')
+    NewListItem.textContent = minutes + ":" + String(seconds).padStart(2, "0") + ":" + milliseconds/10
+    MyList.appendChild(NewListItem)
+}
